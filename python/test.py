2779890f620b298807eb5e537be216370a3628a0
Robert Treacy <rtreacy@hmdc.harvard.edu>
2021-03-04 10:53:52 -0500

    added test that uses make_bounded_sum_l2, make_base_gaussian and make_count_l1

--- python/test.py
+++ python/test.py
@@ -47,5 +47,35 @@ def main():
     odp.data.data_free(ret)
     odp.core.measurement_free(everything)
 
+    split_dataframe = odp.trans.make_split_dataframe(b",", 3)
+    parse_column_1 = odp.trans.make_parse_column(b"<i32>", b"1", True)
+    parse_column_2 = odp.trans.make_parse_column(b"<f64>", b"2", True)
+    parse_dataframe = odp.make_chain_tt_multi(parse_column_2, parse_column_1, split_dataframe)
+
+    select_3a = odp.trans.make_select_column(b"<i32>", b"1")
+    clamp_3 = odp.trans.make_clamp(b"<i32>", odp.i32_p(0), odp.i32_p(10))
+    bounded_sum_3 = odp.trans.make_bounded_sum_l2(b"<i32>", odp.i32_p(0), odp.i32_p(10))
+    base_gaussian_1 = odp.meas.make_base_gaussian(b"<i32>", 1.0)
+    noisy_sum_3 = odp.core.make_chain_mt(base_gaussian_1, odp.make_chain_tt_multi(bounded_sum_3, clamp_3, select_3a))
+
+    select_3 = odp.trans.make_select_column(b"<f64>", b"2")
+    var = odp.trans.make_create_dataframe(3)
+    count_3 = odp.trans.make_count_l1(b"<f64>")
+    base_gaussian = odp.meas.make_base_gaussian(b"<u32>",1)
+    noisy_count_3 = odp.core.make_chain_mt(base_gaussian, odp.make_chain_tt_multi(count_3, select_3))
+    composition = odp.core.make_composition(noisy_sum_3, noisy_count_3)
+    everything = odp.core.make_chain_tt(composition, parse_dataframe)
+
+    data1 = odp.data.from_string(b"ant, 1, 1.1\nbat, 2, 2.2\ncat, 3, 3.3")
+    ret = odp.core.measurement_invoke(everything, data1)
+    print(odp.to_str(ret))
+
+    # Clean up
+    odp.data.data_free(arg)
+    odp.data.data_free(ret)
+    odp.core.measurement_free(everything)
+
+
+
 if __name__ == "__main__":
     main()
